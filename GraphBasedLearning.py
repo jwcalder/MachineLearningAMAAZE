251b0800c02aba09ff07b7f17464220f24da30e7
EricChen <eric.h.chen2023@gmail.com>
2022-04-07 20:16:49 -0500

    Created .csv export code.

--- GraphBasedLearning.py
+++ GraphBasedLearning.py
@@ -1,23 +1,45 @@
 import graphlearning as gl
 
 from utils import frag_level_ml_dataset
+import sys
 
 data,target,specimens,target_names = frag_level_ml_dataset()
+
 
 #GL Semisupervised, with ss
 from sklearn import preprocessing
 data = preprocessing.StandardScaler().fit_transform(data)
 
 W = gl.weightmatrix.knn(data,10, similarity = 'angular')
-
-#x is the percentage of training data
-x = 0.75
-train_ind = gl.trainsets.generate(target, rate=x)
-train_labels = target[train_ind]
+def getaccuracyGL(x):
+    train_ind = gl.trainsets.generate(target, rate=x)
+    train_labels = target[train_ind]
 
 
-model = gl.ssl.laplace(W)
-pred_labels = model.fit_predict(train_ind, train_labels)
+    model = gl.ssl.laplace(W)
+    pred_labels = model.fit_predict(train_ind, train_labels)
 
-accuracy = gl.ssl.ssl_accuracy(pred_labels, target, len(train_ind))
-print("Accuracy: %.2f%%"%accuracy)
+    accuracy = gl.ssl.ssl_accuracy(pred_labels, target, len(train_ind))
+    return accuracy
+#We will list percentages of training data from 5% to 75%. We will run each percentage 500 times, and get the average of those 500 trials.
+sys.stdout = open('results/gbl_ml_accuracies_angular.csv', 'w')
+x = 0
+print('Percentage_Training_Data', end = ', ')
+for i in range(500):
+    print('Trial %d'%(i+1), end = ', ')
+print('Average_Accuracy')
+for i in range(15):
+    avgAcc = 0
+    x += 0.05
+    x = round(x, 2)
+    print(x, end = ', ')
+    for i in range(500):
+        acc = getaccuracyGL(x)
+        avgAcc += acc
+        print(acc, end = ', ')
+    print(avgAcc/500.0, end = '')
+    print('')
+sys.stdout.close()
+
+
+
