63c0aece9d433ed5e7c3aa34ff1a0921ad9b216c
EricChen <eric.h.chen2023@gmail.com>
2022-04-12 23:47:03 -0500

    Updated outputs to be in results folder

--- accuracy_percentage_plots.py
+++ accuracy_percentage_plots.py
@@ -8,12 +8,19 @@ plt.rcParams.update({
     "font.sans-serif": ["Helvetica"],
     "font.size": 12})
 styles = ['^-', 'o-', 'd-', 's-', 'p-', 'x-', '*-']
+
 data = pd.read_csv('results/gbl_ml_accuracies_angular.csv')
 x = np.array([data.iloc[:,0],data.iloc[:,501]])
 plt.plot(x[0]*100,x[1], marker = 'o', label = 'Angular Weight Matrix')
+
 data = pd.read_csv('results/gbl_ml_accuracies_euclidean.csv')
 x = np.array([data.iloc[:,0],data.iloc[:,501]])
 plt.plot(x[0]*100,x[1], marker = 'D', label = 'Euclidean Weight Matrix')
+
+data = pd.read_csv('results/rf_ml_accuracies.csv')
+x = np.array([data.iloc[:,0], data.iloc[:,1]])
+plt.plot(x[0]*100,x[1]*100,marker = 's', label = 'Random Forest')
+
 plt.xlabel('Percentage Training Data')
 plt.ylabel('Average Accuracy (Percentage)')
 plt.title('Average accuracy of GBL VS. percentage training data')
--- ml_accuracies_generator.py
+++ ml_accuracies_generator.py
@@ -22,6 +22,7 @@ def getaccuracyGL(x):
     accuracy = gl.ssl.ssl_accuracy(pred_labels, target, len(train_ind))
     return accuracy
 #We will list percentages of training data from 5% to 75%. We will run each percentage 500 times, and get the average of those 500 trials.
+#Angular GBL
 sys.stdout = open('results/gbl_ml_accuracies_angular.csv', 'w')
 x = 0
 print('Percentage_Training_Data', end = ', ')
@@ -33,13 +34,14 @@ for i in range(15):
     x += 0.05
     x = round(x, 2)
     print(x, end = ', ')
-    for i in range(500):
+    for j in range(500):
         acc = getaccuracyGL(x)
         avgAcc += acc
         print(acc, end = ', ')
     print(avgAcc/500.0, end = '')
     print('')
 sys.stdout.close()
+#Euclidean GBL
 sys.stdout = open('results/gbl_ml_accuracies_euclidean.csv', 'w')
 W = gl.weightmatrix.knn(data,10)
 x = 0
@@ -52,13 +54,33 @@ for i in range(15):
     x += 0.05
     x = round(x, 2)
     print(x, end = ', ')
-    for i in range(500):
+    for j in range(500):
         acc = getaccuracyGL(x)
         avgAcc += acc
         print(acc, end = ', ')
     print(avgAcc/500.0, end = '')
     print('')
 sys.stdout.close()
-
-
-
+#Random Forest
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+sys.stdout = open('results/rf_ml_accuracies.csv', 'w')
+rf = RandomForestClassifier(n_estimators = 100)
+x = 0
+print('Percentage_Training_Data', end=', ')
+print('Average_Accuracy', end = '')
+print('')
+for i in range (15):
+    x += 0.05
+    x = round(x,2)
+    print(x, end = ', ')
+    avgAcc = 0
+    for j in range(50):
+        X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=1 - x)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_test)
+        avgAcc += metrics.accuracy_score(y_test, y_pred)
+    avgAcc/= 50.0
+    print(avgAcc)
+sys.stdout.close()
