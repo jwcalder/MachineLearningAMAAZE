d3a3108453701c6028b3195aee76a4c0c348e55a
EricChen <eric.h.chen2023@gmail.com>
2022-04-13 21:17:56 -0500

    Updated outputs to be in results folder

--- SpectralEmbedding.py
+++ SpectralEmbedding.py
@@ -13,21 +13,26 @@ data = preprocessing.StandardScaler().fit_transform(data)
 
 W = gl.weightmatrix.knn(data,10)
 
-vec = gl.spectral_embedding(W,4,method='NgJordanWeiss')
-#vec is an nx2 dimensional array.
-#Can plot vec with matplotlib.pyplot as plt.
-ax = plt.gca()
-ax.set_xlim([0.04,0.05])
-ax.set_ylim([0,100])
-print(vec[:, 1:])
-plt.ion()
-plt.scatter(vec[:,1], vec[:, 2], c = target)
-plt.savefig('/Users/EricChen/Documents/Python/realdataplot.jpg')
-plt.figure()
+avgAcc = 0
+for i in range(100):
+  model = gl.clustering.spectral(W,num_clusters = 5,method='NgJordanWeiss')
 
-ax = plt.axes(projection ="3d")
-ax.scatter3D(vec[:,1],vec[:,2],vec[:,3],c=target,s=10)
-for angle in range(0, 360):
-  ax.view_init(30, angle)
-  plt.draw()
-  plt.pause(.001)
+  pred_labels= model.fit_predict()
+  accuracy = gl.clustering.clustering_accuracy(pred_labels,target)
+  avgAcc += accuracy
+print(avgAcc/100.0)
+
+G = gl.graph(W)
+vals,vec = G.eigen_decomp(normalization = 'normalized')
+
+plt.scatter(vec[:,1], vec[:, 2], c = target)
+plt.savefig('figures/spectral_embedding')
+
+plt.show()
+
+#ax = plt.axes(projection ="3d")
+#ax.scatter3D(vec[:,1],vec[:,2],vec[:,3],c=target,s=10)
+#for angle in range(0, 360):
+  #ax.view_init(30, angle)
+  #plt.draw()
+  #plt.pause(.001)
