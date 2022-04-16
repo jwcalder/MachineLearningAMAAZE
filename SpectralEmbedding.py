56697fe10ae43d3567993581fc9c5d96fbc6684b
EricChen <eric.h.chen2023@gmail.com>
2022-04-15 19:48:47 -0500

    Updated outputs to be in results folder

--- SpectralEmbedding.py
+++ SpectralEmbedding.py
@@ -22,12 +22,21 @@ for i in range(100):
   avgAcc += accuracy
 print(avgAcc/100.0)
 
+#Graph Settings
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    "font.sans-serif": ["Helvetica"],
+    "font.size": 12})
+styles = ['^-', 'o-', 'd-', 's-', 'p-', 'x-', '*-']
+
 #Creating graphs from spectral embedding
 G = gl.graph(W)
 vals,vec = G.eigen_decomp(normalization = 'normalized')
 
 #2D graph
 plt.scatter(vec[:,1], vec[:, 2], c = target)
+plt.title('Spectral Embedding, 2D')
 plt.savefig('figures/spectral_embedding_2D')
 plt.show()
 
@@ -35,5 +44,6 @@ plt.show()
 ax = plt.axes(projection ="3d")
 ax.scatter3D(vec[:,1],vec[:,2],vec[:,3],c=target,s=10)
 ax.view_init(30, 60)
+plt.title('Spectral Embedding, 3D')
 plt.savefig('figures/spectral_embedding_3D')
 plt.show()
--- tSNE_embedding.py
+++ tSNE_embedding.py
@@ -6,10 +6,19 @@ from utils import frag_level_ml_dataset
 
 data,target,specimens,target_names = frag_level_ml_dataset()
 
+#Graph Settings
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    "font.sans-serif": ["Helvetica"],
+    "font.size": 12})
+styles = ['^-', 'o-', 'd-', 's-', 'p-', 'x-', '*-']
+
 #T-SNE embedding, 2D
 data_embedded = TSNE(n_components = 2, learning_rate = 'auto', init = 'random').fit_transform(data)
 
 plt.scatter(data_embedded[:,0],data_embedded[:,1], c = target)
+plt.title('TSNE, 2D')
 plt.savefig('figures/tsne_2D')
 plt.show()
 
@@ -20,5 +29,6 @@ data_embedded = TSNE(n_components = 3, learning_rate = 'auto', init = 'random').
 ax = plt.axes(projection ="3d")
 ax.scatter3D(data_embedded[:,0],data_embedded[:,1],data_embedded[:,2],c=target,s=10)
 ax.view_init(30, 60)
+plt.title('TSNE, 3D')
 plt.savefig('figures/tsne_3D')
 plt.show()
